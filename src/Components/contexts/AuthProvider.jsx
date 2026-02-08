1eec1336cdf62394f50628c813029cd8be9567b1
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2026-02-08 13:57:58 +0600

    Added signIn Authentication

--- src/Components/contexts/AuthProvider.jsx
+++ src/Components/contexts/AuthProvider.jsx
@@ -1,16 +1,32 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { AuthContext } from "./AuthContext";
 import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider=({children})=>{
     const[user,setUser]=useState(null)
     const[loading,setLoading]=useState(true)
+    
     const createUser=(email,password)=>{
         setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
     }
+
+    const signInUser=(email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser)
+        })
+        return()=>{
+            unsubscribe()
+        }
+    },[])
     const authInfo={
           createUser,
+          signInUser,
           user,
           loading
     }

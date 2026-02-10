6f07277a6381d44933709411c680c21a3472d438
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2026-02-10 10:32:49 +0600

    Added Home page

--- src/Components/LatestMovies/LatestMovies.jsx
+++ src/Components/LatestMovies/LatestMovies.jsx
@@ -1,20 +1,20 @@
-import { use } from "react";
+import { useEffect, useState } from "react";
 import Movie from "../../Movie/Movie";
 
-const LatestMovies=({latestMoviesPromise})=>{
-    const movies=use(latestMoviesPromise)
-    console.log(movies)
-    return(
-        <div>
-            <h2 className="text-3xl text-green-700 font-semibold text-center pb-4">Latest Movies</h2>
-            <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {
-                movies.map(movie=><Movie
-                    key={movie._id} movie={movie}
-                />)
-            }
-            </div>
-        </div>
-    )
-}
+const LatestMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/latest-movies")
+      .then(res => res.json())
+      .then(data => setMovies(data));
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {movies.map(movie => <Movie key={movie._id} movie={movie} />)}
+    </div>
+  );
+};
+
 export default LatestMovies;
